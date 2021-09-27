b49d4eac8dc64ce24823ae48432ef9e055fe338d
Danielcaos <danielcaos@ufps.edu.co>
2021-09-27 17:05:20 -0500

    validacion de politicas

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function eleccionSi() {
+    if (document.getElementById('checkbox1')) {
+        window.location="preguntas.html";
+    } else {
+      var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+        var alertTrigger = document.getElementById('eleccionsi')
+
+        function alert(message, type) {
+            var wrapper = document.createElement('div')
+            wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-danger" role="alert">' + message
+
+            alertPlaceholder.append(wrapper)
+        }
+
+        if (alertTrigger) {
+            alertTrigger.addEventListener('click', function () {
+                alert('Para continuar, acepta nuestros términos y políticas', 'success')
+            })
+        }  
+
+     }
+}
